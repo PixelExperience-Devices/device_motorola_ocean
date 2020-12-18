1cbb6f96fe74af27ab15b94837307cee99d63bd8
Luiz Dores <luizhenrique.pvh2000@gmail.com>
2020-12-18 16:52:12 -0300

    Revert "ocean: Switch to walleye fingerprint"
    This reverts commit 60713bbb48b2b5ff8e21d0a9162728772e42c5ed.

--- aosp_ocean.mk
+++ aosp_ocean.mk
@@ -37,4 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
         PRIVATE_BUILD_DESC="ocean-user 10 QPO30.52-29 c0490 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
